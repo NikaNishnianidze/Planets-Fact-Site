614e4d123d71e7b8c590f7c394859baaba3c2df4
NikaNishnianidze <nikanishno8@gmail.com>
2025-03-28 13:01:16 +0400

    create header layout with navigation for every page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,57 @@
+import { use, useState } from "react";
+import hamburgerIcon from "../../public/assets/icon-hamburger.svg";
+import imageIcon from "../../public/assets/icon-chevron.svg";
+import { Link } from "react-router-dom";
+import data from "../data.json";
+
 export default function Header() {
-  return <></>;
+  const [show, setShow] = useState<boolean>(false);
+
+  const toggleNavigation = () => {
+    setShow(!show);
+  };
+
+  return (
+    <>
+      <header className="flex flex-col py-[16px] px-[24px] border-b-[1px] border-b-[#FFFFFF]/20">
+        <div className="main-header flex flex-row justify-between items-center">
+          <h1 className="text-[#FFFFFF] text-[28px] font-normal tracking-[-1.05px]">
+            THE PLANETS
+          </h1>
+          <img
+            src={hamburgerIcon}
+            alt="hamburger icon"
+            onClick={toggleNavigation}
+          />
+        </div>
+      </header>
+      <nav
+        className={`${show ? "block" : "hidden"} px-[24px] pt-[24px] mb-[67px]`}
+      >
+        <ul>
+          {data.map((planet) => {
+            return (
+              <div className="planents flex flex-row justify-between items-center  border-b-[1px] border-b-[#FFFFFF]/20 py-[20px]">
+                <div className="planet-name flex flex-row gap-[25px]">
+                  <img
+                    style={{
+                      width: planet.images.sizes.mobileNavigation.width,
+                      height: planet.images.sizes.mobileNavigation.height,
+                    }}
+                    src={planet.images.planet}
+                  />
+                  <li key={planet.name}>
+                    <Link to={planet.name}>{planet.name}</Link>
+                  </li>
+                </div>
+                <div className="arrow">
+                  <img src={imageIcon} alt="chevron icon" />
+                </div>
+              </div>
+            );
+          })}
+        </ul>
+      </nav>
+    </>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Planet from "./components/Planet";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/:planetName",
+        element: <Planet />,
+      },
+    ],
   },
 ]);
 
